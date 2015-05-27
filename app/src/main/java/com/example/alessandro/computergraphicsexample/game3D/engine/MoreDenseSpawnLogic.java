73e00bf60e607e280f063b299bd4a2024e7a192b
maxzambelli <m.zambe93@gmail.com>
2015-05-27 22:04:44 +0200

    Refactoring e documentazione delle logiche di spawning.

--- app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/engine/MoreDenseSpawnLogic.java
+++ app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/engine/MoreDenseSpawnLogic.java
@@ -11,42 +11,58 @@ import model.spawning.SpawnLogic;
 
 /**
  * @author Max
+ * Logica di spawning che si basa sull'utilizzo di variabili casuali gaussiane per generare le coordinate dei nuovi mob.
+ * Tali variabili sono centrate nei valori x e y della ship ed hanno una deviazione standard stdev specificata.
  */
 public class MoreDenseSpawnLogic implements SpawnLogic  {
 
-    private static final float MAX_MOB_SPEED = 0.6f;
-    private static final float STANDARD_DEVIATION = 5.5f;
-    private static final int N = 4;
+    public static final float MOB_MAX_SPEED = 0.6f;
+    public static final float MOB_INITIAL_SPEED = 0.12f;
+    public static final float MOB_SPEED_INCREMENT = 0.0001f;
+    public static final double MOB_COLLISION_RAY = 0.5;
+    public int simultaneousMobs;
+    private float stdev;
     private float initialZ;
-    private Random r = new Random();
+    private Random r;
     private float mobSpeed;
     private Ship ship;
 
-    public MoreDenseSpawnLogic(Ship ship, float initialZ) {
-        this.initialZ = initialZ;
+    /**
+     * @param ship Ship comandata dall'utente
+     * @param stdev Deviazione standard della gaussiana utilizzata per lo spawning
+     * @param simultaneousMobs Numero di mob che spawnano contemporaneamente a seguito di una chiamata a spawnMob()
+     * @param initialZ Coordinata Z iniziale dei nuovi mob
+     */
+    public MoreDenseSpawnLogic(Ship ship, float stdev, int simultaneousMobs, float initialZ) {
         this.ship = ship;
+        this.stdev = stdev;
+        this.simultaneousMobs = simultaneousMobs;
+        this.initialZ = initialZ;
 
-        mobSpeed = 0.12f;
+        r = new Random();
+        mobSpeed = MOB_INITIAL_SPEED;
     }
 
+    /**
+     * Spawna un insieme di mob e restituisce un vettore di reference ad essi.
+     * @return un vettore di reference ai mob istanziati
+     */
     @Override
     public Mob[] spawnMob() {
         Coordinate coord = ship.getCoordinate();
 
-
-        Mob[] mobs = new Mob[ N ];
+        Mob[] mobs = new Mob[simultaneousMobs];
         for (int i = 0; i < mobs.length ; i++) {
 
-            float x = STANDARD_DEVIATION * (float)r.nextGaussian() + coord.getX();
-            float y = STANDARD_DEVIATION * (float)r.nextGaussian() + coord.getY();
+            float x = stdev * (float)r.nextGaussian() + coord.getX();
+            float y = stdev * (float)r.nextGaussian() + coord.getY();
             Coordinate newMobCoord = new Coordinate( x, y, initialZ);
             mobs[i] = new Mob3D(newMobCoord, mobSpeed);
-            mobs[i].setCollisionRay(0.5);
-
+            mobs[i].setCollisionRay(MOB_COLLISION_RAY);
         }
 
-        if(mobSpeed < MAX_MOB_SPEED) {
-            mobSpeed += 0.0001f;
+        if(mobSpeed < MOB_MAX_SPEED) {
+            mobSpeed += MOB_SPEED_INCREMENT;
         }
 
         return mobs;
--- app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/engine/SimpleSpawnLogic.java
+++ app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/engine/SimpleSpawnLogic.java
@@ -10,33 +10,48 @@ import model.spawning.SpawnLogic;
 
 /**
  * @author Max
+ * Una semplice logica di spawning, che utilizza variabili casuali uniformi per selezionare le coordinate di spawn.
+ * Non permette lo spawn simultaneo di più mob.
  */
 public class SimpleSpawnLogic implements SpawnLogic  {
 
-    private static final float MAX_MOB_SPEED = 0.6f;
+    public static final float MOB_MAX_SPEED = 0.6f;
+    public static final float MOB_INITIAL_SPEED = 0.12f;
+    public static final float MOB_SPEED_INCREMENT = 0.0001f;
+    public static final double MOB_COLLISION_RAY = 0.5;
     private float widthBound;
     private float heightBound;
     private float initialZ;
-    private Random r = new Random();
+    private Random r;
     private float mobSpeed;
 
+    /**
+     * @param widthBound Coordinata massima ammessa, lungo l'asse x
+     * @param heightBound Coordinata massima ammessa, lungo l'asse y
+     * @param initialZ Coordinata Z iniziale dei nuovi mob
+     */
     public SimpleSpawnLogic(float widthBound, float heightBound, float initialZ) {
         this.widthBound = widthBound;
         this.heightBound = heightBound;
         this.initialZ = initialZ;
 
-        mobSpeed = 0.05f;
+        r = new Random();
+        mobSpeed = MOB_INITIAL_SPEED;
     }
 
+    /**
+     * Spawna un mob e restituisce un vettore unitario, contenente il reference al mob istanziato.
+     * @return un vettore unitario contenente il reference al mob istanziato
+     */
     @Override
     public Mob[] spawnMob() {
         Mob[] mob = new Mob[1];
         Coordinate newMobCoord = new Coordinate( r.nextFloat() * widthBound * 2 - widthBound, r.nextFloat() * heightBound * 2 - heightBound, initialZ);
         mob[0] = new Mob3D(newMobCoord, mobSpeed);
-        mob[0].setCollisionRay(0.5);
+        mob[0].setCollisionRay(MOB_COLLISION_RAY);
 
-        if(mobSpeed < MAX_MOB_SPEED) {
-            mobSpeed += 0.0001f;
+        if(mobSpeed < MOB_MAX_SPEED) {
+            mobSpeed += MOB_SPEED_INCREMENT;
         }
 
         return mob;
