ed7f7a0cc8afe963c286146a477dddd8e6d80948
maxzambelli <m.zambe93@gmail.com>
2015-05-27 22:21:43 +0200

    Refactoring e documentazione dei componenti per la gestione dell'audio.

--- app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/audio/AudioManager.java
+++ app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/audio/AudioManager.java
@@ -5,15 +5,21 @@ import android.content.res.Resources;
 import java.util.HashMap;
 
 /**
- * Created by Max on 26/05/2015.
+ * @author Max, Bianca
+ * Componente che si occupa di contenere i riferimenti alle risorse audio, mappandole con identificatori alfanumerici.
  */
-public class AudioManager {
+public class AudioManager {  //SINGLETON
 
     private static AudioManager audioManager;
     private HashMap<String, Integer> audioResources = new HashMap<>();
 
-    private AudioManager() {};
+    private AudioManager() {}
 
+    /**
+     * Fornisce l'istanza di AudioManager.
+     * Essendo un singleton, questo metodo è l'unico modo per poter accedere all'istanza correntemente in uso.
+     * @return istanza di AudioManager
+     */
     public static AudioManager getInstance() {
         if(audioManager == null) {
             audioManager = new AudioManager();
@@ -21,15 +27,27 @@ public class AudioManager {
         return audioManager;
     }
 
+    /**
+     * Aggiunge una risorsa audio, associandole un identificatore.
+     * Non è possibile associare lo stesso identificatore a risorse diverse, pertanto una risorsa mappata con un nome già presente non verrà inserita.
+     * @param name nome da associare
+     * @param res riferimento alla risorsa da mappare
+     */
     public void addResource(String name, int res) {
-        audioResources.put(name, res);
+        if(!audioResources.keySet().contains(name)) {
+            audioResources.put(name, res);
+        }
     }
 
+    /**
+     * Fornisce il riferimento alla risorsa corrispondente al nome specificato.
+     * @param name identificativo della risorsa da recuperare
+     * @return riferimento alla risorsa recuperata
+     */
     public int getResource(String name) {
         if(audioResources.keySet().contains(name)) {
             return audioResources.get(name);
-        }
-        else
+        } else
             throw new Resources.NotFoundException();
     }
 }
--- app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/audio/AudioPlayer.java
+++ app/src/main/java/com/example/alessandro/computergraphicsexample/game3D/audio/AudioPlayer.java
@@ -4,28 +4,43 @@ import android.content.Context;
 import android.media.MediaPlayer;
 
 /**
- * Created by Max on 26/05/2015.
+ * @author Max, Bianca
+ * Player per risorse audio. Fa riferimento al componente AudioManager per reperire le risorse richieste.
+ * @see AudioManager
  */
 public class AudioPlayer {
 
     private MediaPlayer player;
 
+    /**
+     * @param context Contesto
+     * @param name Nome della risorsa da associare al player. Viene fornita da AudioManager
+     * @param loop Specifica se la traccia debba essere eseguita il loop oppure no
+     */
     public AudioPlayer(Context context, String name, boolean loop) {
-
         player = MediaPlayer.create(context, AudioManager.getInstance().getResource(name));
         player.setLooping(loop);
     }
 
+    /**
+     * Riproduce la traccia associata al player.
+     */
     public void play() {
         player.start();
     }
 
+    /**
+     * Ferma la riproduzione della traccia associata.
+     */
     public void stop() {
         if(player.isPlaying()) {
             player.stop();
         }
     }
 
+    /**
+     * Mette in pausa la riproduzione della traccia associata.
+     */
     public void pause() {
         if(player.isPlaying()) {
             player.pause();
